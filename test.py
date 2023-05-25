c96cb83bbb0a328f4bbfa9bb887abf307d4e1d12
mdlewis33 <114125152+mdlewis33@users.noreply.github.com>
2023-05-25 09:05:36 -0400

    testing

--- test.py
+++ test.py
@@ -1,3 +1,2 @@
-import numpy as np
-
-print("hello world")
+def test1():
+    return "Hello World!"
---
+++ test_with_pytest.py
@@ -0,0 +1,4 @@
+from test import test1
+
+def test_function():
+    assert "Hello World!" == test1()
